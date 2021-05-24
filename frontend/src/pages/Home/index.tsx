e8f0eb7b948a2f0715fe170b53fb97bcb403aeca
Reinaldo.Rosa <rei16703@gmail.com>
2021-05-24 10:52:44 -0300

    Home,navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,24 @@
+import { Link } from "react-router-dom";
+import NavBar from "src/components/navbar";
 
 
 const Home = () => {
     return (
-        <div >
-           <h1>Home</h1>
-        </div>
+        <>
+            <div className="container">
+
+                <NavBar />
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas</p>
+                    <hr></hr>
+                    <p>Esta aplicação consiste em exiber um dashboard</p>
+
+                    <Link className="btn btn-primary btn-lg" to ="/dashboard">Dashboard</Link>
+                </div>
+            </div>
+            <footer/>
+        </>
     );
 }
 
