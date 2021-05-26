111eb85585c4711c93ce174af67e468cc7bd356d
Reinaldo.Rosa <rei16703@gmail.com>
2021-05-26 09:25:32 -0300

    donutchart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'src/types/Sale';
 import { BASE_URL } from 'src/utils/requests';
@@ -10,18 +11,22 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    let charData: ChartData = {labels: [],series: []};
+    const [ charData, setChartData] = useState<ChartData>({labels: [],series: []});
 
-    axios.get(`${BASE_URL}/vendas/amount-by-seller`)
-         .then( response => {
-             const data = response.data as SaleSum[];
-             const myLabels = data.map(x => x.sellerName);
-             const myseries = data.map(x => x.sum);
+    useEffect (() => {
+        axios.get(`${BASE_URL}vendas/amount-by-seller`)
+        .then( response => {
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const myseries = data.map(x => x.sum);
 
-             charData = {labels: myLabels,series: myseries};
+            setChartData({labels: myLabels,series: myseries});
 
-         }
-         );
+        }
+        );
+    },[] )
+
+
 
 /*     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
