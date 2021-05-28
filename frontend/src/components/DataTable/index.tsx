75fc7dcba042eeb605b4a864f2c3a1712c7965e0
Reinaldo.Rosa <rei16703@gmail.com>
2021-05-28 11:24:12 -0300

    datatable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,5 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "src/types/Sale";
+import { formatLocalDate } from "src/utils/format";
+import { BASE_URL } from "src/utils/requests";
 
 const DataTable = () => {
+
+  const [page, setPage] = useState<SalePage>({
+    
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    number: 0,
+    first: true,
+
+  });
+
+  useEffect(() => {
+
+       axios.get(`${BASE_URL}vendas?page=0&size=20&sort=date,desc`)
+          .then( response => {
+
+            setPage(response.data);
+
+          });
+
+  }, [] );
+
 return (
 <div className="table-responsive">
     <table className="table table-striped table-sm">
@@ -13,13 +40,19 @@ return (
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
+            {page.content?.map(item => (
+                        <tr key={item.id}>
+                        <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                        <td>{item.seller.name}</td>
+                        <td>{item.visited}</td>
+                        <td>{item.deals}</td>
+                        <td>{item.amount}</td>
+                    </tr>
+
+            )) };
+
+
+    
         </tbody>
     </table>
 </div>);
--- frontend/src/types/Sale.ts
+++ frontend/src/types/Sale.ts
@@ -1,6 +1,36 @@
+import { Seller } from './Seller' 
+
 export type SaleSum = {
 
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
     
+}
+
+export type SalesSuccess  = {
+    deals: number;
+    visited: number;
+    sellerName: string;
+}
+
+export type Sale = {
+
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: Date;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
 }
